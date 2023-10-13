6b1a5cd5d89043de4d5ad16a913fd991ad688de2
makeitvsolo <makeitvsolo@gmail.com>
2023-10-13 13:34:34 +0300

    feat: bspwm now starts after login

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+if status is-login
+	if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+		exec startx -- -keeptty
+	end
 end
+
